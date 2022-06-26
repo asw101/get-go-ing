eecc4cdce9e3f84e435ab976e2653aff036ca388
Aaron Wislang <aaron.wislang@microsoft.com>
2022-06-26 13:39:02 -0400

    echo dirs
    Signed-off-by: Aaron Wislang <aaron.wislang@microsoft.com>

--- build-images.sh
+++ build-images.sh
@@ -1,12 +1,17 @@
 # echo "DOCKER_LABELS: ${DOCKER_LABELS}"
 
-for d in */ ; do
-    TAG="ghcr.io/${GITHUB_ACTION_REPOSITORY}/${d}:latest"
-    
-    docker build \
-        -t "$TAG" \
-        --label "$DOCKER_LABELS" \
-        "$d/"
-
-    docker push "$TAG"
+for d in */; do
+    echo "$d"
 done
+
+
+# for d in */ ; do
+#     TAG="ghcr.io/${GITHUB_ACTION_REPOSITORY}/${d}:latest"
+    
+#     docker build \
+#         -t "$TAG" \
+#         --label "$DOCKER_LABELS" \
+#         "$d/"
+
+#     docker push "$TAG"
+# done
