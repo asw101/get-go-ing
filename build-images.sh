4e0c5e176f3d71667d16a75755aaa5da660eb31c
Aaron Wislang <aaron.wislang@microsoft.com>
2022-06-26 14:01:22 -0400

    Add multiple branches
    Signed-off-by: Aaron Wislang <aaron.wislang@microsoft.com>

--- build-images.sh
+++ build-images.sh
@@ -2,8 +2,12 @@
 
 
 for d in $(ls -d */ | sed 's#/##'); do
-    TAG="ghcr.io/${GITHUB_ACTION_REPOSITORY}/${d}:latest"
     
+    TAG="ghcr.io/${GITHUB_ACTION_REPOSITORY}/${d}:${GIT_BRANCH}"
+    if [ "$GIT_BRANCH" -eq "main" ]; then
+        TAG="ghcr.io/${GITHUB_ACTION_REPOSITORY}/${d}:latest"
+    fi
+
     docker build \
         -t "$TAG" \
         --label "$DOCKER_LABELS" \
